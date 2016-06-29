87687d44df1d7d5542a16e43596027ad05233475
swfree <skye.free@gmail.com>
2016-06-29 14:46:03 -0700

    Add more api endpoints for random qs

--- data.js
+++ data.js
@@ -16,5 +16,32 @@ module.exports = [
     "category": "css",
     "tags": [ "trivia", "css" ],
     "timestamp": "Fri Jun 24 2016 15:14:04 GMT-0700 (PDT)"
+  },
+  {
+    "id": 3,
+    "question": "What's pseudoclassical inheritance?",
+    "answer": "Well there is no classical system in JavaScript, so here we have an alternative blech...",
+    "type": "short answer",
+    "category": "javascript",
+    "tags": [ "trivia", "javascript" ],
+    "timestamp": "Fri Jun 24 2016 16:14:04 GMT-0700 (PDT)"
+  },
+  {
+    "id": 4,
+    "question": "What's flexbox do ?",
+    "answer": "best grid system in CSS3. Use ratios for responsive design.",
+    "type": "short answer",
+    "category": "css",
+    "tags": [ "trivia", "css" ],
+    "timestamp": "Fri Jun 24 2016 15:30:04 GMT-0700 (PDT)"
+  },
+  {
+    "id": 5,
+    "question": "What are some benefits of react vs angular?",
+    "answer": "react components are awesome for scale and modularized code and such and such",
+    "type": "short answer",
+    "category": "javascript",
+    "tags": [ "trivia", "javascript" ],
+    "timestamp": "Fri Jun 24 2016 16:45:00 GMT-0700 (PDT)"
   }
 ];
--- server.js
+++ server.js
@@ -5,11 +5,47 @@ var questions = require('./data.js');
 
 app.use(cors());
 
-app.get('/', function(req, res) {
+app.use(express.static('public'));
+
+app.get('/questions', function(req, res) {
   res.send(questions);
+});
+
+app.get('/random', function(req, res) {
+  var randomIndex = Math.floor(Math.random()*questions.length);
+  res.send(questions[randomIndex]);
+});
+
+app.get('/random/javascript', function(req, res) {
+  var filterCriteria = 'javascript';
+  var filteredQuestions = questions.filter(function(question) {
+    return question.category === filterCriteria;
+  });
+
+  var randomIndex = Math.floor(Math.random()*filteredQuestions.length);
+  res.send(filteredQuestions[randomIndex]);
+});
+
+app.get('/random/css', function(req, res) {
+  var filterCriteria = 'css';
+  var filteredQuestions = questions.filter(function(question) {
+    return question.category === filterCriteria;
+  });
+
+  var randomIndex = Math.floor(Math.random()*filteredQuestions.length);
+  res.send(filteredQuestions[randomIndex]);
+});
+
+app.get('/random/html', function(req, res) {
+  var filterCriteria = 'html';
+  var filteredQuestions = questions.filter(function(question) {
+    return question.category === filterCriteria;
+  });
+
+  var randomIndex = Math.floor(Math.random()*filteredQuestions.length);
+  res.send(filteredQuestions[randomIndex]);
 });
 
 app.listen(3000, function() {
   console.log('app listening on port 3000...');
 });
-
